c9a05a2653aded0945c6bf8a60720beb96418523
Pierre-Jean Fichet <pierrejean.fichet@posteo.net>
2018-04-20 22:53:57 +0200

    To have an usable game loop, we use a non blocking queue.get.

--- eveil/game.py
+++ eveil/game.py
@@ -14,30 +14,34 @@ class Game():
         self.db = Data("data.db")
         self.parser = Parser()
 
-    def run(self):
-        while self.loop == True:
-            self.tick()
-            sleep(.1)
-
-    def tick(self):
-        # check queue
-        client, kind, message = self.queue.get() 
-        if message == "shutdown":
-            self.shutdown()
-        elif kind == "connect" and client not in self.clients:
-            self.clients.append(client)
-            player = Player(self.db, client)
-            client.setPlayer(player)
-        elif kind == "disconnect" and client in self.clients:
-            self.clients.remove(client)
-        else:
-            self.parser.parse(client.player, message)
-        self.queue.task_done()
-    
-    def handleMessage(self, client, message):
-        client.sendMessage(message)
-   
     def shutdown(self):
         self.loop = False
         self.db.close()
-   
+ 
+    def run(self):
+        while self.loop == True:
+            self.parsequeue()
+            sleep(.1)
+
+    def parsequeue(self):
+        # check queue
+        try:
+            # With False, the queue does not block the program
+            # It raises Queue.Empty if empty.
+            client, kind, message = self.queue.get(False) 
+        except queue.Empty:
+            kind = None
+        if kind is not None:
+            if message == "shutdown":
+                self.shutdown()
+            elif kind == "connect" and client not in self.clients:
+                self.clients.append(client)
+                player = Player(self.db, client)
+                client.setPlayer(player)
+            elif kind == "disconnect" and client in self.clients:
+                self.clients.remove(client)
+            else:
+                self.parser.parse(client.player, message)
+            self.queue.task_done()
+      
+  
