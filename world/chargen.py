33bb43296ade3ee10fe4ecaace9449f35cb30414
Pierre-Jean Fichet <pierrejean.fichet@posteo.net>
2018-05-17 09:53:02 +0200

    More universal chargen rooms. Work in progress.

--- world/chargen.py
+++ world/chargen.py
@@ -4,75 +4,81 @@ from eveil.template import Template
 
 # We create the first room 
 room1 = game.map.new_room()
-room1.shortdesc = "le sanctuaire des noms"
+room1.shortdesc = "le gouffre"
 room1.longdesc = Template("""
 <h3>{{room.shortdesc|capitalize}}</h3>
-<p>Au fond d'une profonde vallée, sous l'ombre d'arbres
-centenaires, en partie recouvertes de mousse, gîsent deux vieilles
-pierres. Le peu de lumière perçant le feuillage illumine les runes
-gravées sur leurs
-{% if character.name %}
-    flancs. {{character.name}} y reconnaît son nom, parmis de nombreux autres:
-{% else %}
-    flancs:
-{% endif %}
-Les noms de ceux qui vivèrent et de ceux qui vivront. L'une des
-pierres porte les noms des hommes, l'autre ceux des femmes.</p>
+<p>Au fin fond du gouffre, le cœur sombre de la terre n'est peuplé que
+d'ombres dormantes. Du profond de leur sommeil, les ombres sont habitées
+d'une rumeur: Il est dit que c'est du fin fond du gouffre, que naissent
+les âmes. Parfois, une ombre, s'éveille au son de
+cet appel. Et celle qui n'était qu'ombre parmis les ombres, se reconnaît
+un nom, un genre, une apparence... Et se reconnaît vivante.</p>
 <ul>
-    <li>Pour lire les exemples de noms entrez
-    <code>voir hommes</code>,
-    <code>voir femmes</code>.</li>
-    <li>Pour choisir le genre de votre personnage, entrez:
-    <code>genre: [homme|femme]</code>.
+
+{% if character.name %}
+    <li>L'ombre se reconnaît un nom: {{character.name}}</li>
+{% else %}
     <li>Pour nommer votre personnage, entrez
-    <code>nom: <i>nom_choisi</i></code>.</li>
+    <code>nom <i>nom_choisi</i></code>.</li>
+{% endif %}
+
+{% if character.gender %}
+    <li>
+    {% if character.name %}
+        {{character.name}}
+    {% else %}
+        L'ombre
+    {% endif %}
+    reconnaît qu'{{character.pronoun('il')}}
+    est {{character.pronoun('un')}}
+    {{character.GENDERS[character.gender]}}.
+    </li>
+{% else %}
+    <li>Pour choisir son genre entrez:
+    <code>genre [homme|femme]</code>.</li>
+{% endif %}
+
+{% if character.shortdesc %}
+    <li>{{character.pronoun('il')}} apparaît ainsi:
+    {{character.shortdesc}}</li>
+{% else %}
+    <li>Pour définir son apparence, entrez:
+    <code>apparence <i>quelques mots</i></code>.</li>
+{% endif %}
+
+{% if character.longdesc %}
+    <li>{{character.longdesc}}</li>
+{% else %}
+    <li>Pour définir sa longue description, entrez:
+    <code>description <i>Longue description...</i></code>.</li>
+{% endif %}
+
+<li>Entrez <code>voir</code> lorsque vous avez fini.</li>
 </ul>
-<p>
-{% for link in room.targets %}
-En {{link.dynadesc}}, {{character.name}} peut rejoindre {{link.target.shortdesc}}.
-{% endfor %}
-</p>
-""", {'capitalize': str.capitalize})
+
+{% if character.name and character.gender %}
+{% if character.shortdesc and character.longdesc %}
+    <p>
+    {% for link in room.targets %}
+        En {{link.dynadesc}}, {{character.name}} peut rejoindre {{link.target.shortdesc}}.
+    {% endfor %}
+    </p>
+{% endif %}
+{% endif %}
+""", {'capitalize': str.capitalize,})
 
 # We create an empty second room to link it with the first
 room2 = game.map.new_room()
 link1 = game.map.new_link(room1, room2)
-link1.dynadesc = "s'enfonçant dans la forêt"
+link1.dynadesc = "s'élevant vers la surface"
 link2 = game.map.new_link(room2, room1)
-link2.dynadesc = "approchant de la lisière de la forêt"
+link2.dynadesc = "descendant dans les profondeurs du gouffre"
 
 # Second room description
-room2.shortdesc = "une petite mare"
+room2.shortdesc = "Une caverne"
 room2.longdesc = Template("""
 <h3>{{room.shortdesc|capitalize}}</h3>
-<p>Dans un creu formé par les racines entortillées d'un veil arbre, l'eau de
-pluie s'est accumulée en une petite mare. Profonde et sombre, la lumière ne la
-transperce pas, mais s'y reflète. En s'approchant de son rebors,
-{{character.name}} voit son apparence miroiter.
-{% if character.shortdesc %}
-    À première vue, @character.pronom découvre {{character.shortdesc}}.
-{% endif %}
-{% if character.longdesc %}
-En y regardant de plus près, {{character.pronom}} voit:</p>
-    <blockquote>{{character.longdesc}}</blockquote>
-{% else %}
-    </p>
-{% endif %}
-
-<ul>
-    <li>Pour décrire {{character.name}}
-    <code>description: <i>Longue description...</i></code>.</li>
-    <li>Pour définir la courte description de {{character.name}},
-    entrez: <code>apparence: <i>quelques mots</i></code>.</li>
-    <li>Pour voir {{character.name}}, entrez:
-    <code>voir: <i>{{character.name}}</i></code>.</li>
-</ul>
-<p>
-{% for link in room.targets %}
-En {{link.dynadesc}}, {{character.name}} peut rejoindre {{link.target.shortdesc}}.
-{% endfor %}
-</p>
-
+<p>TODO</p>
 """, {"capitalize": str.capitalize})
 
 
