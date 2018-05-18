4958c863389cecf1a893a7d992c723bcdfe98143
Pierre-Jean Fichet <pierrejean.fichet@posteo.net>
2018-05-18 10:22:05 +0200

    Add some prototype for items.

--- eveil/item.py
+++ eveil/item.py
@@ -13,8 +13,84 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-class Thing():
+class Item():
 
     def __init__(self):
-        self.desc = None
-    
+        self.id = None
+        self.shortdesc = None
+        self.longdesc = None
+        self.attributes = []
+
+    def add_attribute(self, attribute):
+        self.attributes.append(attribute)
+
+    def has_attribute(self, name):
+        for attribute in self.attributes:
+            if attribute.name == name:
+                return True
+        return False
+
+    def get_attribute(self, name):
+        for attribute in self.attributes:
+            if attribute.name == name:
+                return attribute
+        return None
+
+
+class AtContainer():
+
+    def __init__(self, item):
+        self.item = item
+        self.name="container"
+        self.max_volume = 10
+        self.volume = 0
+        self.items = []
+
+    def add_item(self, item):
+        vol = 0
+        volume =  item.get_attribute(volume)
+        if volume:
+            vol = volume.volume
+        if self.volume + vol <= self.max_volume:
+            return False
+        self.volume = self.volume + vol
+        self.items.append(item)
+        return True
+        
+    def rem_item(self, item):
+        if item not in self.items:
+            return False
+        vol = 0
+        volume = item.get_attribute(volume)
+        if volume:
+            vol = volume.volume
+        self.volume = self.volume - vol
+        self.items.remove(item)
+        return True
+
+
+class AtQuality():
+    QUALITIES = [ "rough", "normal", "fine" ]
+
+    def __init__(self, item):
+        self.item = item
+        self.name="quality"
+        self.quality = 0
+
+
+class AtValue():
+
+    def __init__(self, item):
+        self.item = item
+        self.name="value"
+        self.value = 0
+
+
+class AtWear():
+    LEVELS = [ "underwear", "outerwear", "cloak" ]
+
+    def __init__(self, item):
+        self.item = item
+        self.name="wear"
+        self.level = 1
+
