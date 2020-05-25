b92a3aba17a7b52853d3d3fa906bbd72da026639
Pierre-Jean Fichet <pierrejean.fichet@posteo.net>
2020-05-25 17:54:01 +0200

    Add a room in chargen to explain basic commands.

--- world/chargen.py
+++ world/chargen.py
@@ -1,8 +1,6 @@
 from eveil.template import Template
 from eveil.character import SHADOW
 
-# Character generation rooms
-
 # We create the first room 
 room1 = game.map.new_room()
 room1.shortdesc = "la création du personnage"
@@ -52,9 +50,42 @@ link1_2.dynadesc = "passant à la seconde étape"
 link2_1 = game.map.new_link(room2, room1)
 link2_1.dynadesc = "retournant à la première étape"
 
-# Third room description
-room2.shortdesc = "la création des vêtements"
+# Second room description
+room2.shortdesc = "les commandes de base"
 room2.longdesc = Template("""
+<p>Vous pouvez déplacer {{character.name.capitalize}} d'un espace à un
+autre avec la commande
+<code>aller vers <i>mot clé</i></code>. Pour voir à nouveau la
+description de l'environnement (ce texte), utilisez
+<code>regarder</code>. Cette même commande sert à regarder quelqu'un
+ou quelque chose: 
+    {% if character.name == shadow %}
+    <code>regarder <i>mot clé</i></code>.
+    {% else %}
+    <code>regarder <i>{{character.name}}</i></code> par exemple.
+    {% endif %}
+</p>
+<p>La commande la plus utilisée est <code>exposer</code>. Cette commande
+vous permet de d'exposer les actions, gestes, émotions et paroles de
+votre personnage. En son sein, les phrases entre guillemets sont des paroles.
+Les mots-clé <code>/il</code> et <code>/elle</code> sont
+remplacés par le nom pour les personnages qui connaissent votre
+personnage et par sa courte description pour les autres. De la même
+manière, pour référer un autre personnage, utilisez <code>/</code>
+devant son nom.</p>
+</ul>
+""", {'capitalize': str.capitalize, 'shadow': SHADOW})
+
+# We create an empty third room to link it with the second
+room3 = game.map.new_room()
+link2_3 = game.map.new_link(room2, room3)
+link2_3.dynadesc = "passant à la troisième étape"
+link3_2 = game.map.new_link(room3, room2)
+link3_2.dynadesc = "retournant à la seconde étape"
+ 
+# Third room description
+room3.shortdesc = "la création des vêtements"
+room3.longdesc = Template("""
 <p>{{character.name.capitalize}} peut créer se propres vêtements avec
 la commande <code>item vêtement</créer></code>. Ensuite, la commande
 <code>def</code> permet de définir les propriétés d'un item. <code>def
