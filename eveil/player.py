be63c2aedf792b7cbed38e1bc578e0590cdc9f5e
Pierre-Jean Fichet <pierrejean.fichet@posteo.net>
2020-06-04 22:02:23 +0200

    typo fix in the introductio message.

--- eveil/player.py
+++ eveil/player.py
@@ -45,7 +45,7 @@ account_menu = Template("""
     <p>Creez un personnage en entrant <code>créer <i>nom_du_personnage</i></code>.</p>
 {% endif %}
 <p>Vous pouvez aussi modifier ici les données de votre compte avec
-les commandes suivantes:</p>
+les commandes suivantes :</p>
 <ul>
     <li><code>pseudo <i>nouveau_pseudonyme</i></code></li>
     <li><code>secret <i>ancien_mdp nouveau_mdp</i></code></li>
