4cb6edadc271de30ac77ac43e34553735a94a99e
Pierre-Jean Fichet <pierrejean.fichet@posteo.net>
2020-06-08 20:50:00 +0200

    Bug fix: import timedelta.

--- eveil/player.py
+++ eveil/player.py
@@ -13,7 +13,7 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import crypt
 
 from .template import Template
